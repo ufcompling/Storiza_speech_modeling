7a8f40a90c7195bdfdf23c97546b4559e5b4d849
michaelbennie <michaelbennie@ufl.edu>
2025-09-09 23:09:56 -0400

    fixed directory naming

--- scripts/1_word_annotation/directoryConfig.py
+++ scripts/1_word_annotation/directoryConfig.py
@@ -1,11 +1,12 @@
 # ---- Paths (edit these if your storage layout changes) ----
 import os
 ONEDRIVE_DIR = "~/OneDrive/" # can also use "/Users/liu.ying/University of Florida/"; please don't put random hardcoded paths in the middle of the script; you can put them at the bottom or top to make it easier for other people to use
-PROJECT_SUBDIR = "Leite,Walter - Storiza Corpus Spring 2025"
+PROJECT_SUBDIR = "Leite,Walter's files - Storiza Corpus Spring 2025"
 SESSION_SUBDIR = "Storiza_Participant_Recording_05-30-25"
 
 # Base folders
 AUDIO_BASE_DIR = os.path.join(ONEDRIVE_DIR, PROJECT_SUBDIR, SESSION_SUBDIR)
+PROCESSED_UTTERANCE_DIR = os.path.join(ONEDRIVE_DIR, PROJECT_SUBDIR,"Processed_Utterances")
 ROOT_DIR = os.path.join("..", "..")  # repo root (relative to this script)
 PROCESSED_DIR = os.path.join(ROOT_DIR, "processed_data")
 FIXES_DIR = os.path.join(ROOT_DIR, "fixes")
@@ -28,7 +29,7 @@ WORD_LEVEL_NGRAM_CSV = os.path.join(PROCESSED_DIR, "word_level_data_ngram.csv")
 FORMATTED_FULL_CSV = os.path.join("..", "..", "processed_data", "formatted_annotations_with_comments.csv")
 
 # Audio slice output folders (written alongside the source audio session folder)
-SENTENCE_SEGMENTS_DIR = os.path.join(AUDIO_BASE_DIR, "annotated_sentence_segments")
-WORD_SEGMENTS_DIR = os.path.join(AUDIO_BASE_DIR, "annotated_word_segments")
-WORD_SEGMENTS_NGRAM_DIR = os.path.join(AUDIO_BASE_DIR, "annotated_word_segments_ngram")
+SENTENCE_SEGMENTS_DIR = os.path.join(PROCESSED_UTTERANCE_DIR, "annotated_sentence_segments")
+WORD_SEGMENTS_DIR = os.path.join(PROCESSED_UTTERANCE_DIR, "annotated_word_segments")
+WORD_SEGMENTS_NGRAM_DIR = os.path.join(PROCESSED_UTTERANCE_DIR, "annotated_word_segments_ngram")
 
